b0bdfe5208a39271ccad869b8dd552b758a6784f
Marianne Guisset <marianneguisset@aol.com>
2023-04-04 10:07:05 +0200

    sql modif

--- CulteJava.sql
+++ CulteJava.sql
@@ -3,17 +3,17 @@ INSERT INTO `tapoteur` (`rang_tapoteur`, `numero`, `voie`, `cp`, `ville`, `pays`
 ('compileur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'Matou', 'VofVictory', 'Mathias', 'Vercauteren'),
 ('compileur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'thomas', 'f', 'Thomas', 'Feroldi'),
 ('compileur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'anthony', 'r', 'Anthony', 'Rouyer'),
-('compileur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'crockdur', 'guigui', 'Marianne', 'Guisset')
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'LouveLune', 'MeuteDeter', 'Alicia','Monfort')
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'MaçonFache', 'LeBtpCNul', 'Jordan','Rolland')
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'OursonTeddy', 'CodeMiel', 'Teddy','Bengoto')
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'CuriousVoyager', 'CuriousCasa', 'Thomas','Abrial')
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'SupremeParisian', 'IciCParis', 'Angele','Baudet')
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'Bouibouya', 'GuessAll', 'Lina','Allel')
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'FlashBang', 'Brille', 'Chloe','Desflaches')
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'HommeKotier', 'KotdeMaille', 'Julien','Kot')
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'Gang2Canard', 'ApoCoinLips', 'Elodie','Vanzeveren')
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'NuclearPressF', 'RespectTheNuke', 'Guillaume','Vincent')
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'DeinoRageux', 'Pasta5orus', 'Mathias','Ribeiro')
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'AbsoluCyclist', 'veloLointain', 'Mohammed','Moujahid')
+('compileur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'crockdur', 'guigui', 'Marianne', 'Guisset'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'LouveLune', 'MeuteDeter', 'Alicia','Monfort'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'MaçonFache', 'LeBtpCNul', 'Jordan','Rolland'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'OursonTeddy', 'CodeMiel', 'Teddy','Bengoto'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'CuriousVoyager', 'CuriousCasa', 'Thomas','Abrial'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'SupremeParisian', 'IciCParis', 'Angele','Baudet'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'Bouibouya', 'GuessAll', 'Lina','Allel'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'FlashBang', 'Brille', 'Chloe','Desflaches'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'HommeKotier', 'KotdeMaille', 'Julien','Kot'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'Gang2Canard', 'ApoCoinLips', 'Elodie','Vanzeveren'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'NuclearPressF', 'RespectTheNuke', 'Guillaume','Vincent'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'DeinoRageux', 'Pasta5orus', 'Mathias','Ribeiro'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'AbsoluCyclist', 'veloLointain', 'Mohammed','Moujahid');
 
