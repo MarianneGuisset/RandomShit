fb14fe51e6bcb805886541330400987faa1bff96
Marianne Guisset <marianneguisset@aol.com>
2023-04-05 16:10:29 +0200

    bdd sql

--- CulteJava.sql
+++ CulteJava.sql
@@ -1,19 +1,39 @@
-INSERT INTO `tapoteur` (`rang_tapoteur`, `numero`, `voie`, `cp`, `ville`, `pays`, `date_adhesion`, `login`, `password`, `prenom`, `nom`) VALUES
-('grandDev', '12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'dev', 'dev1', 'Dave', 'Davinson'),
-('compileur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'Matou', 'VofVictory', 'Mathias', 'Vercauteren'),
-('compileur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'thomas', 'f', 'Thomas', 'Feroldi'),
-('compileur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'anthony', 'r', 'Anthony', 'Rouyer'),
-('compileur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'crockdur', 'guigui', 'Marianne', 'Guisset'),
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'LouveLune', 'MeuteDeter', 'Alicia','Monfort'),
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'MaçonFache', 'LeBtpCNul', 'Jordan','Rolland'),
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'OursonTeddy', 'CodeMiel', 'Teddy','Bengoto'),
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'CuriousVoyager', 'CuriousCasa', 'Thomas','Abrial'),
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'SupremeParisian', 'IciCParis', 'Angele','Baudet'),
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'Bouibouya', 'GuessAll', 'Lina','Allel'),
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'FlashBang', 'Brille', 'Chloe','Desflaches'),
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'HommeKotier', 'KotdeMaille', 'Julien','Kot'),
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'Gang2Canard', 'ApoCoinLips', 'Elodie','Vanzeveren'),
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'NuclearPressF', 'RespectTheNuke', 'Guillaume','Vincent'),
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'DeinoRageux', 'Pasta5orus', 'Mathias','Ribeiro'),
-('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'AbsoluCyclist', 'veloLointain', 'Mohammed','Moujahid');
+INSERT INTO `tapoteur` (`rang_tapoteur`, `numero`, `voie`, `cp`, `ville`, `pays`, `date_adhesion`, `login`, `password`, `prenom`, `nom`, `somme_don`, `image_profil`, `all_dons`) VALUES
+('grandDev', '12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'dev', 'azerty', 'Dave', 'Davinson', 0, 'assets/Photos/GrandDev.PNG', 1139);
 
+INSERT INTO `tapoteur` (`rang_tapoteur`, `numero`, `voie`, `cp`, `ville`, `pays`, `date_adhesion`, `login`, `password`, `prenom`, `nom`, `somme_don`, `image_profil`) VALUES
+('compileur', '12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'compil', 'azerty', 'Isabelle', 'Groin', 0, 'assets/Photos/IsabelleGroin.PNG'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'indent', 'azerty', 'Segolene','Battleroyal', 0, 'assets/Photos/SegoleneBattleroyal.PNG'),
+('fidele','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'fidele', 'azerty', 'Delphine','Gelarond', 104, 'assets/Photos/DelphineGelarond.PNG'),
+
+('compileur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'Matou', 'VofVictory', 'Mathias', 'Vercauteren', 0, 'assets/Photos/MathiasVercauteren.PNG'),
+('compileur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'thomas', 'f', 'Thomas', 'Feroldi', 0, 'assets/Photos/ThomasFeroldi.PNG'),
+('compileur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'anthony', 'r', 'Anthony', 'Rouyer', 0, 'assets/Photos/AnthonyRouyer.PNG'),
+('compileur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'crockdur', 'guigui', 'Marianne', 'Guisset', 0, 'assets/Photos/MarianneGuisset.PNG'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'LouveLune', 'MeuteDeter', 'Alicia','Monfort', 0, 'assets/Photos/AliciaMonfort.PNG'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'MaçonFache', 'LeBtpCNul', 'Jordan','Rolland', 0, 'assets/Photos/JordanRolland.PNG'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'OursonTeddy', 'CodeMiel', 'Teddy','Bengoto', 0, 'assets/Photos/TeddyBengoto.PNG'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'CuriousVoyager', 'CuriousCasa', 'Thomas','Abrial', 0, 'assets/Photos/ThomasAbrial.PNG'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'SupremeParisian', 'IciCParis', 'Angele','Baudet', 0, 'assets/Photos/AngeleBaudet.PNG'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'Bouibouya', 'GuessAll', 'Lina','Allel', 1000, 'assets/Photos/LinaAllel.PNG'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'FlashBang', 'Brille', 'Chloe','Desflaches', 0, 'assets/Photos/ChloeDesflaches.PNG'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'HommeKotier', 'KotdeMaille', 'Julien','Kot', 0, 'assets/Photos/JulienKot.PNG'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'Gang2Canard', 'ApoCoinLips', 'Elodie','Vanzeveren', 0, 'assets/Photos/ElodieVanzeveren.PNG'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'NuclearPressF', 'RespectTheNuke', 'Guillaume','Vincent', 0, 'assets/Photos/GuillaumeVincent.PNG'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'DeinoRageux', 'Pasta5aurus', 'Mathias','Ribeiro', 0, 'assets/Photos/MathiasRibeiro.PNG'),
+('indenteur','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'AbsoluCyclist', 'veloLointain', 'Mohammed','Moujahid', 0, 'assets/Photos/MohammedMoujahid.PNG'),
+('fidele','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'aymeric', 'fort', 'Aymeric','Auret', 0, 'assets/Photos/AymericAuret.PNG'),
+('fidele','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'bernard', 'narbert', 'Bernard','Narbert', 0, 'assets/Photos/BernardNarbert.PNG'),
+('fidele','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'berthe', 'salade', 'Berthe','Lacharette', 0, 'assets/Photos/BertheLacharette.PNG'),
+('fidele','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'jean31', '12051981', 'Jean','Viey', 0, 'assets/Photos/JeanViey.PNG'),
+('fidele','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'cricri', '123456789', 'Christelle','Mett', 0, 'assets/Photos/ChristelleMett.PNG'),
+('fidele','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'francis', 'francis', 'Francis','Tatanne', 0, 'assets/Photos/FrancisTatanne.PNG'),
+('fidele','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'remy-fi', 'imfruity', 'Remy','Figuemiraisin', 0, 'assets/Photos/RemyFiguemiraisin.PNG'),
+('fidele','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'ellie-co', 'aviation4ever', 'Ellie','Coptere', 0, 'assets/Photos/EllieCoptere.PNG'),
+('fidele','12', 'rue rouget', '31100', 'toulouse', 'france', '2023-01-19', 'soso12', 'gardien', 'Solange','Gardien', 35, 'assets/Photos/SolangeGardien.PNG');
+
+INSERT INTO `evenement` (`activite`, `date_event`, `lieu`, `nom`) VALUES
+('rassemblement', '2023-09-16', 'Toulouse', 'Anniversaire de la compileuse Marianne Guisset.'),
+('promotion', '2023-05-07', 'Bordeaux', "Passation de pouvoir du Grand Dev."),
+('promotion', '2023-05-29', 'Paris', 'Promotion du fidèle Remy Figuemiraisin.'),
+('pelerinnage', '2023-12-15', 'Paris', 'Pelerinnage.');
